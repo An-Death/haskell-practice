94069677228be700260d5669cd35d8f19919913a
an-death <opiumofthepeople@yandex.ru>
2019-01-15 10:58:10 +0300

    Stepic. Haskell. DataTypes. Task 4.2 Data types with parameters

--- data_types.hs
+++ data_types.hs
@@ -1,6 +1,9 @@
 module Demo where
 
-    
+import Data.Either
+import Data.Char(isDigit)
+import Data.List ( any, all, words, sortOn, elemIndex, isSubsequenceOf)
+import Data.List.Split (splitOn)
 import Data.Time.Clock
 import Data.Time.Format
 import Text.Printf
@@ -126,6 +129,56 @@ abbrFirstName :: Person -> Person
 abbrFirstName p@(Person (x:y:s) _ _) = p {firstName = x : "."}
 abbrFirstName p = p
 
+
+data Error = ParsingError | IncompleteDataError | IncorrectDataError String deriving(Show)
+-- Input : "firstName = John\nlastName = Connor\nage = 30"
+-- split lines by "\n"
+-- If parse failed -> ParsingError
+-- If one of required field for Person not in input -> IncompleteDataError
+-- If age not a number -> IncorrectDataError (value age)
+-- All field outside Person should be ignored
+parsePerson :: String -> Either Error Person
+parsePerson  = recreatePerson . validateFields . filterField . parse 
+    where
+        delim = "\n"
+        personFields = ["lastName", "firstName", "age"]
+        personAge = personFields!!2
+        containDelim = isSubsequenceOf delim
+        parse :: String -> Either Error [String]
+        parse s 
+            | s == "" = Left ParsingError
+            | not . containDelim $ s = Left ParsingError
+            | otherwise = Right (splitOn delim s)
+        
+        
+        filterField :: Either Error [String] -> Either Error [String]
+        filterField (Right dt) = Right fields
+            where
+                fields = filter (\x -> any ($ x) (map isSubsequenceOf personFields)) dt
+        filterField err = err
+
+        
+        validateFields :: Either Error [String] -> Either Error [String]
+        validateFields (Right fields)
+                | length fields < 3 = Left IncompleteDataError
+                | all (not . isDigit) age = Left (IncorrectDataError age) 
+                | otherwise = Right fields
+                where
+                    age = (!!2). words . head . filter (isSubsequenceOf personAge) $ fields 
+        validateFields err = err
+        
+        recreatePerson ::Either Error [String] -> Either Error Person
+        recreatePerson (Right fields) = Right (Person {age=read (f!!0), lastName=f!!1, firstName=f!!2})
+            where
+                f = cleanFields fields
+                cleanFields = map snd . reverse . sortOn fst . map cleanField
+                cleanField field = (pos, value) where
+                    dt = words field
+                    value = dt!!2
+                    pos = elemIndex (dt!!0) personFields
+        recreatePerson (Left err) = Left err
+        
+
 infixl 1 &
 (&) :: a -> (a -> b) -> b
 x & f = f x
@@ -145,3 +198,53 @@ logLevelToString = show
 
 logEntryToString :: LogEntry -> String
 logEntryToString = show
+
+data Coord a = Coord a a
+
+distance' :: Coord Double -> Coord Double -> Double
+distance' (Coord x1 y1) (Coord x2 y2) = sqrt $ (x2 - x1)^2 + (y2-y1)^2
+
+manhDistance :: Coord Int -> Coord Int -> Int
+manhDistance (Coord x1 y1) (Coord x2 y2) =  f x1 x2 + f y1 y2 where
+    f a b = abs $ a-b
+
+-- Center of interesting Cell 
+-- should be  lenCell * X + 1/2*lenCell
+-- other words if X and Y is Int and thay mean the positon of bottom-left point of Cell
+-- lenCell * (X + 0.5)
+-- getCenter 2.2 (Coord 2 1) = Coord 5.5 3.3 
+getCenter :: Double -> Coord Int -> Coord Double
+getCenter lenCell (Coord x y) = Coord (f x) (f y) where 
+    f :: Int -> Double
+    f = (lenCell *) . inc05 . fromIntegral
+    inc05 = (0.5 +)
+
+getCell :: Double -> Coord Double -> Coord Int
+getCell lenCell (Coord x y) = Coord (f x)  (f y) where 
+    f :: Double -> Int
+    f = floor . (/ lenCell)
+
+
+-- roots' :: Double -> Double -> Double -> Either [Char] (Double, Double)
+-- roots' a b c 
+--     | discr >=0 = Right x1 x2
+--     | otherwise = Left "discr is negative. Roots not found"
+--     where   
+--         x1 = helper (-d)
+--         x2 = helper d
+--         helper x = (-b + x) / (2*a)
+--         d = sqrt discr
+--         discr = b^2 - 4*a*c
+
+
+
+findDigit :: [Char] -> Maybe Char
+findDigit [] = Nothing
+findDigit (x:xs) 
+    | isDigit x = Just x
+    | otherwise =  findDigit xs
+
+findDigitOrX :: [Char] -> Char
+findDigitOrX x = case findDigit x of
+    Just a -> a 
+    Nothing -> 'X'
