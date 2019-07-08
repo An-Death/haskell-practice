a1b9d87fedf49b834c5b25d784d1ca4c3fcb1386
an-death <opiumofthepeople@yandex.ru>
2019-07-08 17:12:24 +0300

    Optimized x3 by using ByteString.Lazy.Search

--- logreform.hs
+++ logreform.hs
@@ -8,24 +8,31 @@ import System.CPUTime (getCPUTime)
 import System.Environment (getArgs)
 import Text.Printf
 import System.IO
-import qualified Codec.Compression.GZip        as GZip
-import qualified Data.ByteString.Lazy          as ByteString 
-import qualified Data.ByteString.Lazy.Char8    as ByteString (lines, unlines)
-import qualified Data.ByteString               as ByteStringStrict
+import qualified Codec.Compression.GZip            as GZip
+import qualified Data.ByteString.Lazy              as Lazy 
+import qualified Data.ByteString.Lazy.Char8        as Lazy (lines, unlines)
+import qualified Data.ByteString.Lazy.Search.KMP   as Lazy
+import qualified Data.ByteString                   as Strict
 
 basePath dir = (dropTrailingPathSeparator dir)
 withBase dir = (++) ((basePath dir) ++ [pathSeparator])
 
 isGz file = takeExtension file == ".gz"
 
-patterns :: [ByteStringStrict.ByteString]
-patterns = [" reject:", "client=", "warning: header Subject", "TLS connection established from"]
-filterLines :: [ByteString.ByteString] ->  [ByteString.ByteString]
-filterLines = filter isValid 
+whichReader file
+    | isGz file = readGziped
+    | otherwise = readSimple
     where 
-        isValid x = any ($ ByteString.toStrict (x)) filterPatterns
-        filterPatterns = map ByteStringStrict.isInfixOf patterns
+    readSimple = Lazy.hGetContents
+    readGziped =  fmap GZip.decompress . readSimple
 
+patterns :: [Strict.ByteString]
+patterns =  [" reject:", "client=", "warning: header Subject", "TLS connection established from"]
+filterLines :: [Lazy.ByteString] ->  [Lazy.ByteString]
+filterLines = filter match 
+    where 
+        match x = all ($ x) filterPatterns
+        filterPatterns = map (\pat v -> not . null $ Lazy.indices pat v) patterns
  
 time :: IO t -> IO t
 time a = do
@@ -36,18 +43,11 @@ time a = do
     printf "Computation time: %0.3f sec\n" (diff :: Double)
     return v 
 
-writeCompressed :: FilePath -> ByteString.ByteString -> IO ()
-writeCompressed toFile = write . GZip.compress
-    where
-        write = ByteString.appendFile file
-        file = if isGz toFile then toFile else toFile ++ ".gz"
-
-filterFile :: ByteString.ByteString -> ByteString.ByteString
-filterFile = ByteString.unlines . filterLines . ByteString.lines
-
+filterFile :: Lazy.ByteString -> Lazy.ByteString
+filterFile = Lazy.unlines . filterLines . Lazy.lines
 
 main :: IO()
-main = do
+main =  do
     putStrLn "Starting..."
     [dir, toFile] <- getArgs
     printf "Path: %s -> %s.gz \n" dir toFile
@@ -62,13 +62,7 @@ main = do
                     reader = whichReader file
                 in withFile path ReadMode (\h -> do 
                         content <- reader h
-                        ByteString.hPut wH (GZip.compress $ filterFile content)
+                        Lazy.hPut wH (GZip.compress $ filterFile content)
                 )))
     putStrLn "Done."
 
-whichReader file
-    | isGz file = readGziped
-    | otherwise = readSimple
-    where 
-    readSimple = ByteString.hGetContents
-    readGziped =  fmap GZip.decompress . readSimple
